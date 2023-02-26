583f1cf9ab29b43a53f6ae2c8b80226995cd339c
pc <adrian.valladarez@gmail.com>
2023-02-25 21:25:38 -0600

    pruebaSubida

--- src/app/landing-page/content-main/content-main.component.ts
+++ src/app/landing-page/content-main/content-main.component.ts
@@ -4,7 +4,6 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-content-main',
   templateUrl: './content-main.component.html',
-  
   styleUrls: ['./content-main.component.css']
 })
 export class ContentMainComponent {
