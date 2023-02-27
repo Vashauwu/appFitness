7f252b9c0582ab6e4be3438d6008a3edb99bac71
pc <adrian.valladarez@gmail.com>
2023-02-26 20:36:18 -0600

    subida de aboutUs

--- src/app/landing-page/landing-page.module.ts
+++ src/app/landing-page/landing-page.module.ts
@@ -4,17 +4,22 @@ import { ContentMainComponent } from './content-main/content-main.component';
 import { SharedModule } from '../shared/shared.module';
 import {MatButtonModule} from '@angular/material/button';
 import { RouterModule } from '@angular/router';
+import { AboutUsComponent } from './about-us/about-us.component';
+import { MatCardModule } from '@angular/material/card';
 
 
 
 @NgModule({
   declarations: [
-    ContentMainComponent
+    ContentMainComponent,
+    AboutUsComponent
   ],
   imports: [
     CommonModule,
     SharedModule,
     RouterModule,
+    MatCardModule,
+    MatButtonModule
     
   ],
   exports:[
